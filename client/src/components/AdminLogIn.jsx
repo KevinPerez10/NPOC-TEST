71a57062924542092fcb319eb0258af434f0ef1e
Lau-rence <112626382+Lau-rence@users.noreply.github.com>
2022-09-04 18:47:28 +0700

    AdminLogIn Progress1

--- client/src/components/AdminLogIn.jsx
+++ client/src/components/AdminLogIn.jsx
@@ -1,17 +1,13 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import {BrowserRouter as Link } from 'react-router-dom'
 
-export default function AdminLogIn() {
-    const adminUser = {
-        email: "admin@admin.com",
-        password: "admin123"
-    }
+export default function AdminLogIn({Login, error}) {
+    const [details, setDetails] = useState({email: "", password: ""});
 
-    const [user, setUser] = useState({name: "", email: ""});
-    const [error, setError] = useState("");
+    const submitHandler = e => {
+        e.preventDefault();
 
-    const Login = details => {
-        console.log(details);
+        Login(details);
     }
 
   return (
@@ -27,23 +23,28 @@ export default function AdminLogIn() {
         <div className='text-white h-screen grid place-items-center bg-button-dblue'>
             <div className='min-h-fit w-fit flex flex-col justify-center items-center shadow-lg p-20'>
                 <h2 className="text-center mb-4"> Welcome! Please login to your account </h2>
-                <form className='grid grid-cols-2 gap-4'>
-                    <div class="col-span-2 flex items-center border-b border-gray py-2">
-                        <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                {(error != "") ? (<div className="error">{error}</div>) : ""}
+                <form className='grid grid-cols-2 gap-4' onSubmit={submitHandler}>
+                    <div className="col-span-2 flex items-center border-b border-gray py-2">
+                        <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                                 type="text"
                                 placeholder="Email"
-                                aria-label="email"/>
+                                aria-label="email"
+                                onChange={e => setDetails({...details, email: e.target.value})}
+                                value={details.email}/>
         
                     </div>
-                    <div class="col-span-2 flex items-center border-b border-gray py-2">
-                        <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    <div className="col-span-2 flex items-center border-b border-gray py-2">
+                        <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                                 type="text"
                                 placeholder="Password"
-                                aria-label="password"/>
+                                aria-label="password"
+                                onChange={e => setDetails({...details, password: e.target.value})}
+                                value={details.password}/>
         
                     </div>
-                    <Link to="/dashboard" className='col-start-2'>
-                        <button class="w-full flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded"
+                    <Link /*to="/dashboard"*/ className='col-start-2'>
+                        <button className="w-full flex-shrink-0 bg-gray-500 hover:bg-gray-700 border-gray-500 hover:border-gray-700 text-sm border-4 text-white py-1 px-2 rounded"
                                 type="submit">
                             Log In
                         </button>
--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {BrowserRouter as Link } from 'react-router-dom'
+import AdminLogIn from './AdminLogIn';
 
 export default function Dashboard() {
+    //start
+    const adminUser = {
+        name: "admin1",
+        email: "admin@admin.com",
+        password: "admin123"
+    }
+
+    const [user, setUser] = useState({email: ""});
+    const [error, setError] = useState("");
+
+    const Login = details => {
+        console.log(details);
+
+        if (details.email == adminUser.email && details.password == adminUser.password)
+        {
+            console.log("Logged in");
+            setUser({
+                email: details.email
+            });
+        }else {
+            console.log("Details do not match!");
+            setError("Details do not match!");
+        }
+    }
+
+    const Logout = () => {
+        setUser({email: ""});
+    }
+    //end
   return (
     <div className='grid grid-cols-2 gap-4 place-items-center'>
-        <div className='row-span-3'>
-            Nolasco - Perez Optical Clinic
-        </div>
-        <div className=''>
-            Dashboard
-        </div>
-        <div className=''>
-            Main
-        </div>
-        <div>
-            Footer
-        </div>
+            {(user.email != "") ? (
+                <div className="welcome">
+                    <h1>Welcome, <span>{adminUser.name}</span></h1>
+                    <button>Logout</button>
+                    <div className='row-span-3'>
+                     Nolasco - Perez Optical Clinic
+                    </div>
+                    <div className=''>
+                     Dashboard
+                    </div>
+                    <div className=''>
+                    Main
+                    </div>
+                    <div>
+                     Footer
+                    </div>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+            ) : (
+                <AdminLogIn Login={Login} error={error}/>
+            )}
+
     </div>
   )
 }
